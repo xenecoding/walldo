4c035afa91b2590097768e7dcf45df9a2845bec5
Diego Caceres <dcaceresxene@gmail.com>
2021-10-25 20:03:22 -0300

    182 - validar usuario

--- walldo-app/src/app/login/login.component.ts
+++ walldo-app/src/app/login/login.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
+	selector: 'app-login',
+	templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	user: string = ""
+	pass: string = ""
+	error: boolean = false
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private Router: Router
+	) { }
+
+	ngOnInit(): void {
+	}
+
+	login() {
+		if (this.user == "" || this.pass == "") {
+			this.error = true
+		} else {
+			if (this.user == "admin" && this.pass == "1234") {
+				this.Router.navigateByUrl("panel")
+				this.error = false
+			} else {
+				this.error = true
+
+			}
+		}
+		
+		
+	}
 
 }
